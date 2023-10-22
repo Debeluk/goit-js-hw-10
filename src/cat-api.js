d72e7f64156f33274d2320eebc6d6e60b6ab8b5b
Debeluk <popcorn.starinets@gmail.com>
2023-10-22 21:28:41 +0300

    3st

--- src/cat-api.js
+++ src/cat-api.js
@@ -9,3 +9,16 @@ export function fetchBreeds() {
     return resp.json();
   });
 }
+
+
+export function fetchCatByBreed(breedId) {
+    const BASE_URL = 'https://api.thecatapi.com/v1';
+
+    return fetch(`${BASE_URL}/images/search?breed_ids=${breedId}`).then(respon => {
+        if (!respon.ok) {
+            throw new Error(respon.statusText);
+          }
+
+        return respon.json();
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,36 @@
-import axios from "axios";
-axios.defaults.headers.common["x-api-key"] = "live_7OmHCNWsSrqi3vdA1HFMl0CkV035BNyesghrSC8tUcfriqDjnLMsFYPhRtKIyotP";
+import axios from 'axios';
+import { fetchBreeds,fetchCatByBreed } from './cat-api.js';
 
-const selector = document.querySelector('.breed-select')
+axios.defaults.headers.common['x-api-key'] =
+  'live_7OmHCNWsSrqi3vdA1HFMl0CkV035BNyesghrSC8tUcfriqDjnLMsFYPhRtKIyotP';
 
-import { fetchBreeds } from "./cat-api.js";
+const selector = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
+
+let breedInfo = [];
 
 fetchBreeds()
-.then(data => {
+  .then(data => {
     data.forEach(breed => {
-        const option = document.createElement('option');
-        option.value = breed.id;
-        option.textContent = breed.name;
-        selector.appendChild(option)
+      const option = document.createElement('option');
+      option.value = breed.id;
+      option.textContent = breed.name;
+      selector.appendChild(option);
     });
-})
-.catch(err => console.log(err))
+    breedInfo = data;
+  })
+  .catch(err => console.log(err));
 
-
-
-
+selector.addEventListener('change', event => {
+  const info = breedInfo.find(item => item.id === event.target.value);
+  fetchCatByBreed(event.target.value)
+    .then(data => {
+      catInfo.innerHTML = `
+        <img src="${data[0].url}" alt="${info.name}" width="300" />
+        <h2>${info.name}</h2>
+        <p><strong>Опис:</strong> ${info.description}</p>
+        <p><strong>Темперамент:</strong> ${info.temperament}</p>
+        `;
+    })
+    .catch(err => console.log(err));
+});
