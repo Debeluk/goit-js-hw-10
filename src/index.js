2d6294b29ab06f37d7d69821d8a69b0aced47b5c
Debeluk <popcorn.starinets@gmail.com>
2023-10-23 13:30:46 +0300

    fixes 6st

--- src/index.js
+++ src/index.js
@@ -3,48 +3,60 @@ import Notiflix from 'notiflix';
 import SlimSelect from 'slim-select';
 import { fetchBreeds,fetchCatByBreed } from './cat-api.js';
 
-axios.defaults.headers.common['x-api-key'] =
-  'live_7OmHCNWsSrqi3vdA1HFMl0CkV035BNyesghrSC8tUcfriqDjnLMsFYPhRtKIyotP';
+axios.defaults.headers.common['x-api-key'] = 'live_7OmHCNWsSrqi3vdA1HFMl0CkV035BNyesghrSC8tUcfriqDjnLMsFYPhRtKIyotP';
 
-const selector = document.querySelector('.breed-select');
-const catInfo = document.querySelector('.cat-info');
-const loader = document.querySelector('.loader');
+document.addEventListener('DOMContentLoaded', (event) => {
+  const selector = document.querySelector('.breed-select');
+  const catInfo = document.querySelector('.cat-info');
+  const loader = document.querySelector('.loader');
 
-let breedInfo = [];
+  loader.style.display = 'flex';
+  selector.style.display = 'none';
 
-fetchBreeds()
-  .then(data => {
-    data.forEach(breed => {
-      const option = document.createElement('option');
-      option.value = breed.id;
-      option.textContent = breed.name;
-      selector.appendChild(option);
-    });
-    breedInfo = data;
-  })
-  .catch(err => {
-    Notiflix.Notify.Failure('Oops! Something went wrong loading the breeds! Try reloading the page!');
-    console.error(err);
-  });
+  let breedInfo = [];
 
-selector.addEventListener('change', event => {
-    loader.style.display = 'flex';
-  const info = breedInfo.find(item => item.id === event.target.value);
-  fetchCatByBreed(event.target.value)
+  fetchBreeds()
     .then(data => {
-      catInfo.innerHTML = `
-        <div class="img-div">
-        <img src="${data[0].url}" alt="${info.name}" width="300" />
-        </div>
-        <h2>${info.name}</h2>
-        <h3> Description: ${info.description}</h3>
-        <p> Temperament: ${info.temperament}</p>
-        `;
-        loader.style.display = 'none';
+      data.forEach(breed => {
+        const option = document.createElement('option');
+        option.value = breed.id;
+        option.textContent = breed.name;
+        selector.appendChild(option);
+      });
+      breedInfo = data;
+
+      selector.style.display = 'flex';
+      loader.style.display = 'none';
     })
     .catch(err => {
+      Notiflix.Notify.Failure('Oops! Something went wrong loading the breeds! Try reloading the page!');
+      console.error(err);
+
+      selector.style.display = 'flex';
+      loader.style.display = 'none';
+    });
+
+  selector.addEventListener('change', event => {
+    loader.style.display = 'flex';
+    catInfo.innerHTML = '';
+
+    const info = breedInfo.find(item => item.id === event.target.value);
+    fetchCatByBreed(event.target.value)
+      .then(data => {
+        catInfo.innerHTML = `
+          <div class="img-div">
+            <img src="${data[0].url}" alt="${info.name}" width="300" />
+          </div>
+          <h2>${info.name}</h2>
+          <h3> Description: ${info.description}</h3>
+          <p> Temperament: ${info.temperament}</p>
+        `;
+        loader.style.display = 'none';
+      })
+      .catch(err => {
         Notiflix.Notify.Failure('Oops! Something went wrong loading the cat info! Try reloading the page!');
         console.error(err);
-    });
+        loader.style.display = 'none';
+      });
+  });
 });
-
